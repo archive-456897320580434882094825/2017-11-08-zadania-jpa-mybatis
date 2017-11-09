5a2b648ad019405c7924e9f6450bbf33bb6674b0
Michał Kordulewski <michal@kordulewski.pl>
2017-11-09 04:11:46 +0100

    obraz dockerowy postgresa na potrzeby szkolenia

---
+++ template-postgres/README.md
@@ -0,0 +1,27 @@
+Budowa
+--
+```bash
+docker build -t szkolenie-postgres .
+```
+
+Uruchomienie
+--
+```bash
+docker run --rm -p 5432:5432 --name szkolenie-postgres szkolenie-postgres
+```
+
+Dane logowania
+--
+| user       | haslo            | baza danych |
+|------------|------------------|-------------|
+| postgres   | $8)Gp<[M2f@C`j3L | postgres    |
+| szkolenie  | szkolenie        | szkolenie   |
+| szkolenie1 | szkolenie1       | szkolenie1  |
+| szkolenie2 | szkolenie2       | szkolenie2  |
+| szkolenie3 | szkolenie3       | szkolenie3  |
+| szkolenie4 | szkolenie4       | szkolenie4  |
+| szkolenie5 | szkolenie5       | szkolenie5  |
+| szkolenie6 | szkolenie6       | szkolenie6  |
+| szkolenie7 | szkolenie7       | szkolenie7  |
+| szkolenie8 | szkolenie8       | szkolenie8  |
+| szkolenie9 | szkolenie9       | szkolenie9  |
---
+++ template-postgres/init.sql
@@ -0,0 +1,41 @@
+ALTER USER "postgres" WITH PASSWORD '$8)Gp<[M2f@C`j3L';
+
+CREATE USER "szkolenie" PASSWORD 'szkolenie';
+CREATE DATABASE szkolenie;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie TO szkolenie;
+
+CREATE USER "szkolenie1" PASSWORD 'szkolenie1';
+CREATE DATABASE szkolenie1;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie1 TO szkolenie1;
+
+CREATE USER "szkolenie2" PASSWORD 'szkolenie2';
+CREATE DATABASE szkolenie2;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie2 TO szkolenie2;
+
+CREATE USER "szkolenie3" PASSWORD 'szkolenie3';
+CREATE DATABASE szkolenie3;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie3 TO szkolenie3;
+
+CREATE USER "szkolenie4" PASSWORD 'szkolenie4';
+CREATE DATABASE szkolenie4;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie4 TO szkolenie4;
+
+CREATE USER "szkolenie5" PASSWORD 'szkolenie5';
+CREATE DATABASE szkolenie5;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie5 TO szkolenie5;
+
+CREATE USER "szkolenie6" PASSWORD 'szkolenie6';
+CREATE DATABASE szkolenie6;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie6 TO szkolenie6;
+
+CREATE USER "szkolenie7" PASSWORD 'szkolenie7';
+CREATE DATABASE szkolenie7;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie7 TO szkolenie7;
+
+CREATE USER "szkolenie8" PASSWORD 'szkolenie8';
+CREATE DATABASE szkolenie8;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie8 TO szkolenie8;
+
+CREATE USER "szkolenie9" PASSWORD 'szkolenie9';
+CREATE DATABASE szkolenie9;
+GRANT ALL PRIVILEGES ON DATABASE szkolenie TO szkolenie9;
