d907870381e089510c7aefb5c4dc9cd3780bb84b
Michał Kordulewski <michal@kordulewski.pl>
2017-11-10 08:16:31 +0100

    Testy JPA na SpringBoot

---
+++ template-jpa-springboot/src/main/java/com/example/demojpapostgres/JpaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demojpapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpringBootApplication.class, args);
+	}
+}
---
+++ template-jpa-springboot/src/test/java/com/example/demojpapostgres/JpaSpringBootTests.java
@@ -0,0 +1,37 @@
+package com.example.demojpapostgres;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@org.springframework.test.annotation.Rollback(false)
+@javax.transaction.Transactional
+@ActiveProfiles("h2")
+public class JpaSpringBootTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@PersistenceContext(type = PersistenceContextType.EXTENDED)
+	EntityManager em;
+
+	@Test
+	@javax.transaction.Transactional
+	public void testPostgres() {
+		// prepare
+		Person u1 = new Person();
+		u1.setFirstName("Jan");
+		u1.setLastName("Kowalski");
+		em.persist(u1);
+	}
+
+}
